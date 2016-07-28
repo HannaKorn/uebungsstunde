f4fb5d97e62e01aaebeecfa9d723398e6cef9f63
HannaKorn <hanna.korn@web.de>
2016-07-28 08:45:03 +0200

    ein paar Änderungen

---
+++ uebungsblatt_7_1.rb
@@ -0,0 +1,14 @@
+
+# Array als Zeichenkette
+meinArray = ["Hallo", "Corinna", "Nadine", "Hanna"]
+puts meinArray
+
+#String als Zeichenkette
+
+text =  "Kind"
+laenge = text.length
+laenge.times do |i|
+  puts text[i]    #puts ist verantwortlich dafür dass es untereinander gezeigt wird
+end
+
+#Die Veränderung soll auf GitHub zu sehen sein
